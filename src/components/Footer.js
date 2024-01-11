41a68de9eac483d5165bd938d518244fd7d345cc
sania089 <sania.sarin@somaiya.edu>
2024-01-12 00:35:19 +0530

    last commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import logogym from '../assets/icons/logogym.png';
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
 
-export default Footer
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={logogym} alt="logo" style={{ width: '180px', height: '150px' }} />
+    </Stack>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="20px" textAlign="center" pb="40px">Train hard, stay consistent, be patient.</Typography>
+  </Box>
+);
+
+export default Footer;
--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,26 +1,56 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import { useEffect,useState } from 'react';
 
 import HeroBannerImage from '../assets/images/banner.png';
 
-const HeroBanner = () => (
-  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
-    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
-      Sweat, Smile <br />
-      And Repeat
-    </Typography>
-    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
-      Check out the most effective exercises personalized to you
-    </Typography>
-    <Stack>
-      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
-    </Stack>
-    <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
-      Exercise
-    </Typography>
-    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
-  </Box>
-);
+const captions = [
+  'Sweat now,\nShine later',
+  'Transform your body,\nTransform your life',
+  'Train like a beast, \nLook like a beauty',
+  'Your health is an investment, \nnot an expense',
+  "Stronger than yesterday, \nweaker than tomorrow.",
+  "Your only limit is you.",
+  "Commit to be fit.",
+  "Fitness is a journey,\nnot a destination.",
+  "Make sweat your\nbest accessory.",
+  "Train insane or\nremain the same.",
+  "Wake up, work out,\nlook hot, kick ass.",
+  "Fitness is the key to \na healthy and happy life.",
+  "Sore today,\nstrong tomorrow.",
+  "Eat clean,\ntrain dirty.",
+
+  // Add more captions as needed
+];
+
+const HeroBanner = () => {
+  const [randomCaption, setRandomCaption] = useState('');
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * captions.length);
+    setRandomCaption(captions[randomIndex]);
+  }, []);
+
+  return (
+    <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' }, whiteSpace: 'pre-line'}} mb="23px" mt="30px">
+        {randomCaption}
+      </Typography>
+      <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+        Check out the most effective exercises personalized for you
+      </Typography>
+      <Stack>
+        <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+      </Stack>
+      <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+        Exercise
+      </Typography>
+      <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+    </Box>
+  );
+};
+
+
 
 export default HeroBanner;
